cb492ce03f525aa6824f9a421073b39244dba6ae
Jaime Melis <jmelis@redhat.com>
2019-10-08 22:17:37 +0200

    better handler

--- reporter.py
+++ reporter.py
@@ -9,10 +9,15 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 PROMQL_API_URL = 'https://prometheus.app-sre.devshift.net/api/v1/query'
+TIMERANGE = "7d"
 
 
 def to_mb(v):
     return math.ceil(float(v) / (1024*1024))
+
+
+def to_float(v):
+    return float(v)
 
 
 def round_2(v):
@@ -41,7 +46,7 @@ def promql(query):
 
 
 def store_metrics(metrics_dict, metrics, metric_name,
-                  value_handler=None):
+                  handler=None):
     for info in metrics:
         metric = info['metric']
         value = info['value'][1]
@@ -51,8 +56,8 @@ def store_metrics(metrics_dict, metrics, metric_name,
         app = metric['label_app']
         container = metric.get('container_name')
 
-        if value_handler is not None:
-            value = value_handler(value)
+        if handler is not None:
+            value = handler(value)
 
         key = "/".join([cluster, namespace, app, container])
 
@@ -68,17 +73,17 @@ def main():
                           metric='container_memory_usage_bytes',
                           quantile='0.8',
                           timerange='1h')
-    store_metrics(metrics, mem_usage, 'memory_usage', value_handler=to_mb)
+    store_metrics(metrics, mem_usage, 'memory_usage_q0.8', handler=to_mb)
 
     # memory requests
     mem_reqs_metric = 'kube_pod_container_resource_requests_memory_bytes'
     mem_requests = promql_j2('add-label-app.j2', metric=mem_reqs_metric)
-    store_metrics(metrics, mem_requests, 'mem_requests', value_handler=to_mb)
+    store_metrics(metrics, mem_requests, 'mem_requests', handler=to_mb)
 
     # memory limits
     mem_limits_metric = 'kube_pod_container_resource_limits_memory_bytes'
     mem_limits = promql_j2('add-label-app.j2', metric=mem_limits_metric)
-    store_metrics(metrics, mem_limits, 'mem_limits', value_handler=to_mb)
+    store_metrics(metrics, mem_limits, 'mem_limits', handler=to_mb)
 
     # cpu usage
     cpu_usage_metric = ('namespace_pod_name_container_name:'
@@ -87,18 +92,18 @@ def main():
     cpu_usage = promql_j2('quantile-per-container.j2',
                           metric=cpu_usage_metric,
                           quantile='0.8',
-                          timerange='1h')
-    store_metrics(metrics, cpu_usage, 'cpu_usage', value_handler=round_2)
+                          timerange=TIMERANGE)
+    store_metrics(metrics, cpu_usage, 'cpu_usage_q0.8', handler=round_2)
 
     # cpu requests
     cpu_reqs_metric = 'kube_pod_container_resource_requests_cpu_cores'
     cpu_requests = promql_j2('add-label-app.j2', metric=cpu_reqs_metric)
-    store_metrics(metrics, cpu_requests, 'cpu_requests')
+    store_metrics(metrics, cpu_requests, 'cpu_requests', handler=to_float)
 
     # cpu limits
     cpu_limits_metric = 'kube_pod_container_resource_limits_cpu_cores'
     cpu_limits = promql_j2('add-label-app.j2', metric=cpu_limits_metric)
-    store_metrics(metrics, cpu_limits, 'cpu_limits')
+    store_metrics(metrics, cpu_limits, 'cpu_limits', handler=to_float)
 
     print(json.dumps(metrics))
 
